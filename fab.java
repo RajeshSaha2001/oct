cbf69d45b82208e2896f87fb4bf8ec058aa6b5ea
Rajesh Chandra Saha <55900566+RajeshSaha2001@users.noreply.github.com>
2020-10-16 18:29:32 +0530

    Add files via upload

---
+++ fab.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class d_compareToIgnoreCase
+{
+
+    /* int compareToIgnoreCase(String str)
+    Compares two strings lexicographically, ignoring case differences.
+    
+}
